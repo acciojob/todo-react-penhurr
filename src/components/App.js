7428ecbf822cbd28f24642fc1c7f17642ee469d7
madhur <madhurpanghal0001@gmail.com>
2025-12-03 11:34:38 +0530

    2

--- src/components/App.js
+++ src/components/App.js
@@ -8,24 +8,32 @@ const App = () => {
   const [todos, setTodos] = useState([])
   const [input, setInput] = useState("")
 
-  function addTodo(){
-    setTodos([...todos,{id: Date.now(), task: input}])
+  function addTodo() {
+    setTodos([...todos, { id: Date.now(), task: input }])
     setInput("")
-    
+
 
   }
   console.log(todos)
 
   return (
     <div className="main">
-        {/* Do not remove the main div */}
-        <h1>To-Do List</h1>
-        <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
-        <button type="button" onClick={()=>addTodo()}>Add Todo</button>
-        { todos.length!==0 && 
-        todos.map(todo => 
-        <Todo key={todo.id} id={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)
-        }
+      {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button type="button" onClick={() => addTodo()}>Add Todo</button>
+      {todos.length !== 0 &&
+        <ul>
+          {todos.map(todo =>
+            <li key={todo.id}>
+              <Todo id={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
+            </li>
+          )
+          }
+        </ul>
+      }
+
+
     </div>
   )
 }
